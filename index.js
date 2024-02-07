6619ec65292cab504b23abdffa0cb3900eca65f9
sarahdadkhah <132088853+sarahdadkhah@users.noreply.github.com>
2024-02-07 17:32:37 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const hero = document.querySelector(".hero");
+const heroBoy = document.querySelector(".hero-boy");
+const vilan = document.querySelector(".vilan");
+
+function jump() {
+    if (hero.classList != "animate") {
+        hero.classList.add("animate");
+        vilan.style.animation = "move 1s infinite linear";
+
+
+    }
+
+    setTimeout(function() {
+        hero.classList.remove("animate");
+    }, 300);
+}
+
+document.addEventListener("keydown", function() {
+    jump();
+})
+
+/*let isAlive = setInterval(function() {
+    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+    //   console.log(heroTop)
+}*/
+
+let isAlive = setInterval(function(){
+    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+    //console.log(heroTop);
+    let vilanLeft = parseInt(window.getComputedStyle(vilan).getPropertyValue("left"));
+
+    if(vilanLeft < 40 && vilanLeft > 20 && heroTop <= 130){
+        vilan.style.animation = "none" ;
+        alert("game over.press to restart")
+    }
+},10);
